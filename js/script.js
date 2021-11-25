cb79f44db47fadc1f5a3d8301707cd03a33eab6c
Aleksandr Rozhok <akkelaforest@gmail.com>
2021-11-25 18:55:05 +0300

    first homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log(1);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let question1 = prompt("Один из последних просмотренных фильмов?");
+let question2 = prompt("Насколько оцените его?");
+
+personalMovieDB.movies[question1] = question2;
