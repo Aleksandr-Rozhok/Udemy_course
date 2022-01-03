7599e4ea76550aa3cf86aacbd3df5345868d3cfb
Aleksandr Rozhok <akkelaforest@gmail.com>
2022-01-03 14:21:33 +0300

    Work with XMLHTTPRequest and with post form

--- js/script.js
+++ js/script.js
@@ -209,4 +209,55 @@ window.addEventListener('DOMContentLoaded', () => {
         ".menu .container"
     ).render();
 
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+    const messages = {
+        loading: 'Загрузка...',
+        success: 'Форма отправлена! Ожидайте звонка',
+        failure: 'Что-то пошло не так'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessages = document.createElement('div');
+            statusMessages.classList.add('status');
+            statusMessages.textContent = messages.loading;
+            form.append(statusMessages);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+
+            const formData = new FormData(form);
+
+            const obj = {};
+            formData.forEach((value, key) => {
+                obj[key] = value;
+            });
+            const json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessages.textContent = messages.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessages.remove();
+                    }, 2000);
+                } else {
+                    statusMessages.textContent = messages.failure;
+                }
+            });
+        });
+    }
+
 });
