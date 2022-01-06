c7e7150bd68373a2e1783a97f97b079e8873a496
Aleksandr Rozhok <akkelaforest@gmail.com>
2022-01-06 19:20:36 +0300

    added navigation slider teachers version

--- js/script.js
+++ js/script.js
@@ -400,6 +400,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Teachers version slider 1
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -427,6 +428,24 @@ window.addEventListener('DOMContentLoaded', () => {
         current.textContent = slideIndex;
     }
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.classList.add('dot');
+        dot.setAttribute('data-slide-to', i + 1);
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
     next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0;
@@ -445,6 +464,9 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
 
         sliderInner.style.transform = `translateX(-${offset}px)`;
     });
@@ -468,9 +490,31 @@ window.addEventListener('DOMContentLoaded', () => {
             current.textContent = slideIndex;
         }
 
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+
         sliderInner.style.transform = `translateX(-${offset}px)`;
     });
 
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const sliderTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = sliderTo;
+            offset = offset = +width.slice(0, width.length - 2) * (sliderTo - 1);
+
+            sliderInner.style.transform = `translateX(-${offset}px)`;
+
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
+
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+        });
+    });
 
     // showSlides(slideIndex);
 
